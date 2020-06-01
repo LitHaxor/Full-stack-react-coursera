78027f7b29ce111c369296e40685a3bd7121f484
Hasan Ahamed <officials.hasan@gmail.com>
2020-06-01 22:57:42 +0600

    Media replaced by Cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Nav from './components/Nav'
 import Menu from './components/Menu';
+import {DISHES} from './shared/dishes';
 
+class App extends React.Component{
 
-function App() {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
   return (
-    <div>
-      <Nav/>
-      <Menu/>
-    </div>
-  );
+        <div>
+          <Nav/>
+          <Menu dishes={this.state.dishes}/>
+        </div>
+    );
+  }
 }
 
 export default App;
